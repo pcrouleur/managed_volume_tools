677415b458ed0e4690f7c780c1a9c091d411e090
pcrouleur <vendors@zgoda.com>
2019-07-01 17:21:16 -0600

    Added print snapshot command function

--- mv_info.py
+++ mv_info.py
@@ -1,6 +1,7 @@
 import rubrik_cdm
 import click
 import json
+import base64
 import urllib3
 urllib3.disable_warnings()
 
@@ -19,6 +20,7 @@ def cli(managed_volume, full, state):
         print_managed_volume_state(managed_volume_info)
     elif full:
         print_managed_volume_setup(managed_volume_info)
+        print_managed_volume_snapshot(config['mv_end_user_username'], config['mv_end_user_password'], rubrik.node_ip, managed_volume_info['id'])
     else:
         print_managed_volume_info(managed_volume_info)
 
@@ -64,7 +66,7 @@ def print_managed_volume_setup(managed_volume_info):
         for number, channel in enumerate(managed_volume_info['mainExport']['channels']):
             print("mount {}/{}-ch{}".format(config['nfs_mount_path'], managed_volume_info['name'], number))
         print('-' * 50)
-        print("RMAN channels to use in backup scripts:")
+        print("# RMAN channels to use in backup scripts:")
         for number, channel in enumerate(managed_volume_info['mainExport']['channels']):
             print(
                 "allocate channel ch{} device type disk format '{}/{}-ch{}/%U';".format(number, config['nfs_mount_path'],
@@ -81,6 +83,19 @@ def print_managed_volume_state(managed_volume_info):
         snapshot_state = "Read Only"
     print("The current state of the {} managed volume is {} and the managed volume is {}".format(managed_volume_info['name'], managed_volume_info['state'], snapshot_state))
 
+
+def print_managed_volume_snapshot(username, password, rubrik_ip, managed_volume_id):
+    user_pass = username + ':' + password
+    b_user_pass = user_pass.encode()
+    enc_user_pass = base64.b64encode(b_user_pass).decode()
+    print("# The begin snapshot ReST API command is:")
+    print(
+        "curl -k -X POST -H 'Authorization: Basic {}' 'https://{}/api/internal/managed_volume/{}/begin_snapshot'".format(
+            enc_user_pass, rubrik_ip, managed_volume_id))
+    print("# The end snapshot ReST API command is:")
+    print(
+        "curl -k -X POST -H 'Authorization: Basic {}' 'https://{}/api/internal/managed_volume/{}/end_snapshot'".format(
+            enc_user_pass, rubrik_ip, managed_volume_id))
 
 if __name__ == "__main__":
     cli()
