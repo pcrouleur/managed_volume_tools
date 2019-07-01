5454dc9369d651b3f5ef78ad34cb43f87554d1c4
pcrouleur <vendors@zgoda.com>
2019-07-01 15:44:37 -0600

    Added entry for mv_migrate.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='ManagedVolumeTools',
     version='1.0',
-    py_modules=['managed_volumes'],
+    py_modules=['mv_info', 'mv_user', 'mv_migrate'],
     install_requires=[
         'rubrik_cdm',
         'urllib3',
@@ -13,5 +13,6 @@ setup(
         [console_scripts]
         mv_user=mv_user:cli
         mv_info=mv_info:cli
+        mv_migrate=mv_migrate:main
     '''
 )
