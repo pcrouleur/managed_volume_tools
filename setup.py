88aea7bb39880685dfeae35774135db7b2feebe9
pcrouleur <vendors@zgoda.com>
2019-07-09 17:19:50 -0600

    Added mv_mount.py.

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup
 setup(
     name='ManagedVolumeTools',
     version='1.0',
-    py_modules=['mv_info', 'mv_user', 'mv_migrate'],
+    py_modules=['mv_info', 'mv_user', 'mv_migrate', 'mv_mount'],
     install_requires=[
         'rubrik_cdm',
         'urllib3',
         'Click',
+        'sh',
     ],
     entry_points='''
         [console_scripts]
         mv_user=mv_user:cli
         mv_info=mv_info:cli
         mv_migrate=mv_migrate:main
+        mv_mount=mv_mount:cli
     '''
 )
